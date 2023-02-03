1a52d4fa9e55c0d6f959c325e22e210a7c79606f
EmiliaMiqayelyan <emilik200915@gmail.com>
2023-02-03 18:29:02 +0400

    final chart

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./App.css";
-import ChartRedux from "./redux/ChartRedux"
-// import Table from "./Table";
-// import Chart from "./Chart";
+import Table from "./Table";
+import Chart from "./Chart";
 
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Table />
-      <Chart /> */}
-      <ChartRedux />
+      <Table />
+      <Chart />
     </div>
   );
 }
